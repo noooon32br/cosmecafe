32d2595d32b63f4c62e83623f8cf120ddcc2fed0
EC2 Default User <ec2-user@ip-172-31-25-157.ap-northeast-1.compute.internal>
2023-01-09 08:48:53 +0000

    guestuser機能追加

--- app/controllers/public/cosme_items_controller.rb
+++ app/controllers/public/cosme_items_controller.rb
@@ -1,2 +1,17 @@
 class Public::CosmeItemsController < ApplicationController
+  def new
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,12 @@ Rails.application.routes.draw do
     get 'home/top'
   end
   
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   scope module: :public do
     root to: 'homes#top'
   end
+  
 end
