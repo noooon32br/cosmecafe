6c9b17ba50c07e4e9b663e232362b04b2202d96c
EC2 Default User <ec2-user@ip-172-31-25-157.ap-northeast-1.compute.internal>
2023-01-18 09:17:00 +0000

    会員一覧修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,22 @@
 class Admin::UsersController < ApplicationController
     
   def index
-    @user = User.find(params[:id])
+    @users = User.all
   end
   
   def show
     @user = User.find(params[:id])
   end
+  
+  # def update
+  #   @user = User.find(params[:id])
+  #   @user.update(user_params)
+  #   redirect_to admin_user_path
+  # end
+  
+  private
+  
+  # def user_params
+  #   params.require(:user).premit(:is_banned)
+  # end
 end
